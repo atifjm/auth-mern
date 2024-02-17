2cd3db3f30ebd69d81c5b941c387ddc6aff8bea4
Atif Malik <atifjm@hotmail.com>
2024-02-17 20:49:52 +0500

    Project Completed

---
+++ backend/controllers/auth.js
@@ -0,0 +1,7 @@
+export const register = async(req, res, next) => {
+
+}
+
+export const login = async(req, res, next) => {
+    
+}
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
+import authRoute from "./routes/auth.js"
 
 const app = express()
 dotenv.config()
 
 app.use(cors)
 app.use(express.json())
+
+app.use("/api/auth", authRoute)
 
 const port = process.env.PORT || 8080
 
